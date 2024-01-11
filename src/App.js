dedede2689777f23b221ae86fb97b0c74a6d5469
Aleyna Aktaş <aleynaaktas627@gmail.com>
2024-01-11 23:15:56 +0300

    Add tailwind

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div>Flight Search Application</div>;
+  return (
+    <div className="w-full h-screen bg-zinc-100">
+      <nav className="flex items-center h-24 bg-white">
+        <p className="text-teal-700 font-bold text-3xl ml-12">Flightly</p>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
